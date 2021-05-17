02c413be8af4cfdc4ed9a4c29d96d7c9630c762a
khaos <khaos991213@gmail.com>
2021-05-17 12:59:06 +0800

    change

--- src/index.js
+++ src/index.js
@@ -3,15 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import UserGithub from './GitProfile';
 ReactDOM.render(
   <React.StrictMode>
-  <App />
- </React.StrictMode>,
+    {/* <App /> */}
+    <UserGithub source="https://api.github.com/users/khaos991213"/>
+  </React.StrictMode>,
+  
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
